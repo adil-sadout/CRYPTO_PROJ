f81a130344f11e3d3ae0df08df006bc98bbe32b4
Adil Sadout <adelsadout@hotmail.com>
2022-02-24 13:33:41 +0100

    edited some info

--- src/section/FAQ.js
+++ src/section/FAQ.js
@@ -9,7 +9,7 @@ function FAQ() {
         <div className='px-sm-5 pb-5'>
           <Accordion title="Where can I purchase an NFT?" children="TBD"/>
           <Accordion title="How many NFTs will be available?" children={<p><span className='bold'>The Brokers</span> is a collection consisting of 250 NFTs. This is the 1st out of our multiple phases on our Roadmap.</p>} />
-          <Accordion title="How much will it cost?" children="The mint price will be 0.1 Sol" />
+          <Accordion title="How much will it cost?" children="The mint price will be 0.2 Sol" />
           <Accordion title="How can I get access to the whitelist?" children="All the steps are included in the #how-to-whitelist in our discord group" />
         </div>
         
--- src/section/Landing.js
+++ src/section/Landing.js
@@ -4,7 +4,7 @@ function Landing() {
   return (
     <div className='d-flex p-2 landingHeight text-center flex-column justify-content-center align-items-center text-light  landingbg'>
         <h1 className='display-3'>THE BROKERS</h1>
-        <p>The first community driven project of 250 original NFTs on the Solana blockchain.</p>
+        <p>The first community driven project of original NFTs on the Solana blockchain.</p>
         <button className='btn btn-warning'>Mint Here</button>
         
     </div>
